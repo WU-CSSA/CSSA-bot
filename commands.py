16e7df471edd21114b299d120ac23687d9c4afd0
Jimmy Ostler <jtostler1@gmail.com>
2025-09-27 13:33:56 -0700

    add cache command

--- commands.py
+++ commands.py
@@ -2,6 +2,11 @@
 import discord
 
 async def register_commands(bot, tree) -> None:
+    @tree.command(description="Prints the message cache")
+    async def cache(inter: discord.Interaction) -> None:
+        cache = [*bot.cached_messages]
+        await inter.response.send_message(f"{cache}")
+
     @tree.command(name="ping", description="Tests the bot's latency.")
     async def ping(inter: discord.Interaction) -> None:
         """Get the bot's latency"""
